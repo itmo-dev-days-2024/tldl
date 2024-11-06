5914a40c7f6e4d4021153c8f57dc45b0f19d833a
aalimonov_2 <aalimonov_2@miem.hse.ru>
2024-11-06 13:48:25 +0300

    Upload a bunch of stuff for bot

---
+++ tldl-back/tldl/__init__.py
@@ -0,0 +1,8 @@
+""" TLDL """
+
+__author__ = "Alexey Limonov"
+__email__ = "aa1limonov@yandex.ru"
+__version__ = "0.0.1"
+
+
+__all__ = ("__author__", "__email__", "__version__")
---
+++ tldl-back/tldl/settings.py
@@ -0,0 +1,17 @@
+import os
+from pydantic_settings import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    # App related
+    bot_token: str = os.getenv(
+        "BOT_TOKEN", "7008982832:AAGTOSXd4AY3ZKwdvcnro48lwmxx-ab8pJI"
+    )
+    chat_id: int | str = os.getenv("CHAT_ID", "1003941009")
+    # Databases
+    pg_url: str = os.getenv(
+        "PG_URL", "postgresql+asyncpg://tldl:crackme@localhost:5432/tldl"
+    )
+
+
+settings = AppSettings()
