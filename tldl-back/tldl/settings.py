7732b30d094057b67b2ff8be028da62e50d88ffb
aalimonov_2 <aalimonov_2@miem.hse.ru>
2024-11-07 13:36:57 +0300

    Bot is working

--- tldl-back/tldl/settings.py
+++ tldl-back/tldl/settings.py
@@ -4,10 +4,8 @@ from pydantic_settings import BaseSettings
 
 class AppSettings(BaseSettings):
     # App related
-    tg_api_server: str = os.getenv("TG_API_SERVER", 'http://84.252.132.42')
-    bot_token: str = os.getenv(
-        "BOT_TOKEN", "xxx"
-    )
+    tg_api_server: str = os.getenv("TG_API_SERVER", "http://84.252.132.42")
+    bot_token: str = os.getenv("BOT_TOKEN", "xxx")
     chat_id: int | str = os.getenv("CHAT_ID", "1003941009")
     # Databases
     pg_url: str = os.getenv(
@@ -17,12 +15,13 @@ class AppSettings(BaseSettings):
     s3_endpoint: str = os.getenv("S3_ENDPOINT", "https://storage.yandexcloud.net")
     s3_access_key_id: str = os.getenv("S3_ACCESS_KEY_ID", "xxx")
     s3_secret_access_key: str = os.getenv("S3_SECRET_ACCESS_KEY", "xxx")
-    
-    #LLMs
-    key_ya_gpt = os.getenv('KEY_YA_GPT')
-    folder_ya_gpt = os.getenv('FOLDER_YA_GPT')
-    key_gigachat = os.getenv('KEY_GIGACHAT')
-    
+    # LLMs
+    key_ya_gpt: str = os.getenv("KEY_YA_GPT", "xxx")
+    folder_ya_gpt: str = os.getenv("FOLDER_YA_GPT", "xxx")
+    key_gigachat: str = os.getenv(
+        "KEY_GIGACHAT",
+        "xxx",
+    )
 
 
 settings = AppSettings()
